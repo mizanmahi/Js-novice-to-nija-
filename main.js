9874aace1db173fcc4848425f1fff337c138ca0b
mizanmahi <mizanmahi24@gmail.com>
2019-09-26 18:23:19 -0700

    Variable

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-alert('Welcome to quiz ninja!');
+// Basics of programming 
+
+let name = 'mizan mahi';
+const score = 5;
+
+console.log(name);
+console.log(score);
+
+name = 'miraz muhit';
+console.log(name);
+//score = 10;  //this will through an error
+
+
+console.log(score);
